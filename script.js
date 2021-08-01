1cffc0d1ea760bf8438564ad1545b6174f632f0f
junerae <80211043+junerae@users.noreply.github.com>
2021-08-01 12:27:31 +0200

    verbetering

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function huiswerkMaken(vak, callback) {
     setTimeout(() => {
-        console.log('Ok, ok, ik ga nu mijn ${vak} huiswerk maken')
-    }, 0)
+        console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`)
+        callback()
+    }, 2000);
 }
 
 
